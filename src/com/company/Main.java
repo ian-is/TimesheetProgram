ad00bebee98708dfd541adfdb5bd88597f93604b
Ianis Chisiu <IC210452@reigate.ac.uk>
2021-09-24 12:31:30 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+	System.out.println("enter your name");
+    String name = input.next();
+
+
+    System.out.println("Enter the amount of hours worked this week");
+    double hours = input.nextDouble();
+
+
+    System.out.println("Enter your hourly wage");
+    double wage = input.nextDouble();
+
+    if (hours>45){
+        hours - 45
+    }
+
+
+
+
+
+
+    }
+}
